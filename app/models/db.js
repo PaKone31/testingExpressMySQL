a69686ae0584a9c98421b3bec71d6e64e0174eb4
PaKone <fahr0531@gmail.com>
2020-09-08 05:51:53 -0500

    Add table create

--- app/models/db.js
+++ app/models/db.js
@@ -12,7 +12,36 @@ const connection = mysql.createConnection({
 // open the MySQL connection
 connection.connect(error => {
   if (error) throw error;
+
   console.log("Successfully connected to the database.");
+
+  let createTables = [`CREATE TABLE IF NOT EXISTS payment_orders (
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8`,
+    `CREATE TABLE IF NOT EXISTS articles (
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8`,
+    `CREATE TABLE IF NOT EXISTS payment_orders_articles (
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    quantity int NOT NULL,
+    paymentOrderId int(11) NOT NULL,
+    CONSTRAINT fk_paymentOrder FOREIGN KEY(paymentOrderId) REFERENCES payment_orders(id),
+    articleId int(11) NOT NULL,
+    CONSTRAINT fk_article FOREIGN KEY(articleId) REFERENCES articles(id)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8`
+  ];
+
+  createTables.forEach(itemQuery => {
+    connection.query(itemQuery, function(err, result) {
+      if (err) throw err;
+    });
+  });
+
 });
 
 module.exports = connection;
